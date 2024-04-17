47ed924e20bf2506a2148533a1c9e3268e7011a2
AhmedKamal1911 <khmedamal@gmail.com>
2024-04-17 15:55:29 +0200

    test

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -55,7 +55,7 @@ const Sidebar = () => {
               flexDirection: "column",
               alignItems: "center",
               justifyContent: "center",
-              width: "220px",
+              width: "250px",
               height: "100vh",
               background: "white",
             }}
