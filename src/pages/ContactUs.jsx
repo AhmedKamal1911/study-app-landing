f8cdf7c3bf1a9c58741bbbb897acd39f02518206
AhmedKamal1911 <khmedamal@gmail.com>
2024-09-17 04:41:43 +0300

    feat: fix the form image and the form height

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -188,7 +188,12 @@ const ContactUs = () => {
           {/* Img */}
           <Box flex={1}>
             <img
-              style={{ width: "100%", height: "100%", borderRadius: "8px" }}
+              style={{
+                width: "100%",
+                height: "100%",
+                borderRadius: "8px",
+                objectFit: "cover",
+              }}
               src={contactImg}
               alt=""
             />
@@ -205,6 +210,7 @@ const ContactUs = () => {
             p={{ xs: 5, md: 6 }}
             className="shadow-1"
             borderRadius="8px"
+            minHeight="799.64px"
           >
             <SectionHeader alignment="left" subtitle={"EDUCATION FOR EVERYONE"}>
               <Typography
@@ -218,152 +224,155 @@ const ContactUs = () => {
                 Get a Free Course You Can Contact With Me
               </Typography>
             </SectionHeader>
-            <StyledTextField
-              value={formik.values.name}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              fullWidth
-              id="standard-basic"
-              name="name"
-              label="Name"
-              variant="standard"
-            />
-            <FieldError
-              errorText={
-                formik.touched.name && formik.errors.name
-                  ? formik.errors.name
-                  : null
-              }
-            />
-
-            <StyledTextField
-              value={formik.values.email}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              fullWidth
-              name="email"
-              id="standard-basic"
-              label="Email"
-              variant="standard"
-            />
-            <FieldError
-              errorText={
-                formik.touched.email && formik.errors.email
-                  ? formik.errors.email
-                  : null
-              }
-            />
-
-            <StyledTextField
-              value={formik.values.subject}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              fullWidth
-              id="standard-basic"
-              label="Your Subject"
-              variant="standard"
-              name="subject"
-            />
-            <FieldError
-              errorText={
-                formik.touched.subject && formik.errors.subject
-                  ? formik.errors.subject
-                  : null
-              }
-            />
-
-            <StyledTextField
-              value={formik.values.message}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              id="standard-multiline-static"
-              label="Message"
-              multiline
-              rows={4}
-              variant="standard"
-              name="message"
-            />
-            <FieldError
-              errorText={
-                formik.touched.message && formik.errors.message
-                  ? formik.errors.message
-                  : null
-              }
-            />
-
-            <Button
-              type="submit"
-              disabled={formik.isSubmitting}
-              sx={{
-                display: "flex",
-
-                textAlign: "center",
-
-                p: 1,
-                gap: 1,
-                backgroundImage:
-                  "linear-gradient(to right, #2F57EA,#2F57EA,#b966e7, #b966e7)",
-                backgroundSize: "300% 100%",
-                color: "white",
-                fontWeight: "bold",
-                transition: "all 0.6s ease-in-out",
-                fontSize: "17px",
-                textTransform: "capitalize",
-                "&:hover": {
-                  backgroundPosition: "100% -100%",
-                },
-                "&:hover .left-arrow": {
-                  opacity: "1",
-                  transform: "translateX(10px)",
-                },
-                "&:hover .right-arrow": {
-                  opacity: "0",
-                  transform: "translateX(10px)",
-                },
-                "&:hover .btn-text": {
-                  transform: "translateX(15px)",
-                },
-              }}
-            >
-              <East
-                className="left-arrow"
-                sx={{
-                  fontSize: "18px",
-                  opacity: "0",
-                  transition: "0.5",
-                }}
+            <Box flex={1}>
+              <StyledTextField
+                value={formik.values.name}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                fullWidth
+                id="standard-basic"
+                name="name"
+                label="Name"
+                variant="standard"
               />
-              <span
-                className="btn-text"
-                style={{
-                  transition:
-                    "transform 0.6s 0.125s cubic-bezier(0.1, 0.75, 0.25, 1)",
+              <FieldError
+                errorText={
+                  formik.touched.name && formik.errors.name
+                    ? formik.errors.name
+                    : null
+                }
+              />
+
+              <StyledTextField
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                fullWidth
+                name="email"
+                id="standard-basic"
+                label="Email"
+                variant="standard"
+              />
+              <FieldError
+                errorText={
+                  formik.touched.email && formik.errors.email
+                    ? formik.errors.email
+                    : null
+                }
+              />
+
+              <StyledTextField
+                value={formik.values.subject}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                fullWidth
+                id="standard-basic"
+                label="Your Subject"
+                variant="standard"
+                name="subject"
+              />
+              <FieldError
+                errorText={
+                  formik.touched.subject && formik.errors.subject
+                    ? formik.errors.subject
+                    : null
+                }
+              />
+
+              <StyledTextField
+                value={formik.values.message}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                id="standard-multiline-static"
+                label="Message"
+                multiline
+                rows={4}
+                variant="standard"
+                name="message"
+                fullWidth
+              />
+              <FieldError
+                errorText={
+                  formik.touched.message && formik.errors.message
+                    ? formik.errors.message
+                    : null
+                }
+              />
+
+              <Button
+                type="submit"
+                disabled={formik.isSubmitting}
+                sx={{
                   display: "flex",
-                  alignItems: "center",
+                  mt: "45px",
+                  textAlign: "center",
+
+                  p: 1,
+                  gap: 1,
+                  backgroundImage:
+                    "linear-gradient(to right, #2F57EA,#2F57EA,#b966e7, #b966e7)",
+                  backgroundSize: "300% 100%",
+                  color: "white",
+                  fontWeight: "bold",
+                  transition: "all 0.6s ease-in-out",
+                  fontSize: "17px",
+                  textTransform: "capitalize",
+                  "&:hover": {
+                    backgroundPosition: "100% -100%",
+                  },
+                  "&:hover .left-arrow": {
+                    opacity: "1",
+                    transform: "translateX(10px)",
+                  },
+                  "&:hover .right-arrow": {
+                    opacity: "0",
+                    transform: "translateX(10px)",
+                  },
+                  "&:hover .btn-text": {
+                    transform: "translateX(15px)",
+                  },
                 }}
               >
-                {formik.isSubmitting ? (
-                  <CircularProgress
-                    sx={{
-                      width: "26px !important",
-                      height: "26px !important",
-                      color: "#29e1a9",
-                      marginRight: "10px",
-                    }}
-                  />
-                ) : (
-                  ""
-                )}
-                {formik.isSubmitting ? "Loading..." : "Get it Now"}
-              </span>
-              <East
-                className="right-arrow"
-                sx={{
-                  fontSize: "18px",
-                  transition: "0.3s ",
-                }}
-              />
-            </Button>
+                <East
+                  className="left-arrow"
+                  sx={{
+                    fontSize: "18px",
+                    opacity: "0",
+                    transition: "0.5",
+                  }}
+                />
+                <span
+                  className="btn-text"
+                  style={{
+                    transition:
+                      "transform 0.6s 0.125s cubic-bezier(0.1, 0.75, 0.25, 1)",
+                    display: "flex",
+                    alignItems: "center",
+                  }}
+                >
+                  {formik.isSubmitting ? (
+                    <CircularProgress
+                      sx={{
+                        width: "26px !important",
+                        height: "26px !important",
+                        color: "#29e1a9",
+                        marginRight: "10px",
+                      }}
+                    />
+                  ) : (
+                    ""
+                  )}
+                  {formik.isSubmitting ? "Loading..." : "Get it Now"}
+                </span>
+                <East
+                  className="right-arrow"
+                  sx={{
+                    fontSize: "18px",
+                    transition: "0.3s ",
+                  }}
+                />
+              </Button>
+            </Box>
           </Stack>
         </Stack>
       </Container>
